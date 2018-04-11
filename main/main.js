34e14c7c4446edc53d5094d381bec76fd056d485
maskingtimeflows <1078674571@qq.com>
2018-04-11 17:20:12 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input) {
+    var data=indata(input);
+    var result=printResult(data);
+    return result;
 };
+function indata(input) {
+    if(input==='')
+    {
+        return null;
+    }
+    let x=input.split('+');
+    let data={};
+    data.distance=parseFloat(x[0]);
+    data.min=parseFloat(x[1]);
+    data.price=(data.distance<=2)?6+(0.25*data.min):
+        (data.distance<=8)?6+Math.round((data.distance-2)*0.8+(0.25*data.min)):
+            (data.distance>8)? 6+Math.round((6*0.8)+((data.distance-8)*1.2)+(0.25*data.min)):
+                null;
+    return data;
+}
+function printResult(data) {
+    if(data===null)
+    {
+        return null;
+    }
+    let solte='------------------\n'+'距离：'+data.distance+'km'+'\n'+'等待：'+data.min+'分钟\n'+'收费：'+data.price+'元\n'+'------------------\n';
+    return solte;
+}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,29 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it('低于2公里的收费无等待时间', function() {
+        var input='';
+        var result = main(input);
+        expect(result).toEqual(null);
+    });
+    it('低于2公里的收费无等待时间', function() {
+        var input='1.2km+0min';
+        var result = main(input);
+        expect(result).toEqual('------------------\n'+'距离：1.2km\n'+'等待：0分钟\n'+'收费：6元\n'+'------------------\n');
+    });
+    it('大于2公里小于8公里的收费无等待时间', function() {
+        var input='3km+0min';
+        var result = main(input);
+        expect(result).toEqual('------------------\n'+'距离：3km\n'+'等待：0分钟\n'+'收费：7元\n'+'------------------\n');
+    });
+    it('大于8公里的收费无等待时间', function() {
+        var input='10km+0min';
+        var result = main(input);
+        expect(result).toEqual('------------------\n'+'距离：10km\n'+'等待：0分钟\n'+'收费：13元\n'+'------------------\n');
+    });
+    it('有等待时间的收费', function() {
+        var input='10km+4min';
+        var result = main(input);
+        expect(result).toEqual('------------------\n'+'距离：10km\n'+'等待：4分钟\n'+'收费：14元\n'+'------------------\n');
+    });
 });
